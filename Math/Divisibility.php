816483c27c0952e978ecc05e5e491c8fd09c66bb
Keiwen <romainwald@hotmail.com>
2016-11-14 16:35:26 +0100

    get half, third, quarter

--- Math/Divisibility.php
+++ Math/Divisibility.php
@@ -32,5 +32,61 @@ class Divisibility {
 		return !self::isNumberEven($number);
 	}
 
-	
+
+	/**
+	* @param float $value
+	* @param float $total
+	* @param int   $partCount
+	* @param bool  $floor
+	* @return int
+	*/
+	public static function getValuePartFromTotal(float $value, float $total, int $partCount, bool $floor = true) {
+	$valuePart = $partCount * $value / $total;
+	if($floor) {
+	    $valuePart = floor($valuePart) + 1;
+	} else {
+	    $valuePart = ceil($valuePart);
+	}
+	if($valuePart <= 0) $valuePart = 1;
+	if($valuePart > $partCount) $valuePart = $partCount;
+	return $valuePart;
+	}
+
+
+	/**
+	* @param float $value
+	* @param float $total
+	* @param bool  $floor
+	* @return int
+	*/
+	public static function getHalf(float $value, float $total, bool $floor = true)
+	{
+		return static::getValuePartFromTotal($value, $total, 2, $floor);
+	}
+
+
+	/**
+	* @param float $value
+	* @param float $total
+	* @param bool  $floor
+	* @return int
+	*/
+	public static function getThird(float $value, float $total, bool $floor = true)
+	{
+		return static::getValuePartFromTotal($value, $total, 3, $floor);
+	}
+
+
+	/**
+	* @param float $value
+	* @param float $total
+	* @param bool  $floor
+	* @return int
+	*/
+	public static function getQuarter(float $value, float $total, bool $floor = true)
+	{
+		return static::getValuePartFromTotal($value, $total, 4, $floor);
+	}
+
+
 }
