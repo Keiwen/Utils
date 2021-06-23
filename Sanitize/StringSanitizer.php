b2263c068320d0088e8e0e968ac29a039d747a33
Keiwen <keiwen.mk@gmail.com>
2021-06-23 11:20:58 +0200

    adjust sanitizer

--- Sanitize/StringSanitizer.php
+++ Sanitize/StringSanitizer.php
@@ -6,64 +6,87 @@ namespace Keiwen\Utils\Sanitize;
 class StringSanitizer
 {
 
-	const FILTER_DEFAULT = 'default';
-	const FILTER_BOOLEAN = 'boolean';
-	const FILTER_INT = 'int';
-	const FILTER_FLOAT = 'float';
-	const FILTER_ALPHA = 'alpha';
-	const FILTER_SCALAR = 'scalar';
-	const FILTER_MAIL = 'mail';
-	const FILTER_IP = 'ip';
-	const FILTER_MAC = 'mac';
-	const FILTER_REGEXP = 'regexp';
-	const FILTER_URL = 'url';
-	const FILTER_FIRSTLETTER = 'firstLetter';
-	const FILTER_FIRSTTWOLETTER = 'firstTwoLetter';
-	const FILTER_FIRSTTHREELETTER = 'firstThreeLetter';
-	const FILTER_FIRSTFOURLETTER = 'firstFourLetter';
-	const FILTER_FIRSTFIVELETTER = 'firstFiveLetter';
-    const FILTER_SLUG = 'slug';
-    const FILTER_JSON_ARRAY = 'jsonArray';
-    const FILTER_JSON_OBJECT = 'jsonObject';
-	const FILTER_COLORHEXA= 'colorHexa';
+	public const FILTER_DEFAULT = 'default';
+	public const FILTER_BOOLEAN = 'boolean';
+	public const FILTER_INT = 'int';
+	public const FILTER_FLOAT = 'float';
+	public const FILTER_ALPHA = 'alpha';
+	public const FILTER_SCALAR = 'scalar';
+	public const FILTER_MAIL = 'mail';
+	public const FILTER_IP = 'ip';
+	public const FILTER_MAC = 'mac';
+	public const FILTER_URL = 'url';
+	public const FILTER_FIRSTLETTER = 'firstLetter';
+	public const FILTER_FIRSTTWOLETTER = 'firstTwoLetter';
+	public const FILTER_FIRSTTHREELETTER = 'firstThreeLetter';
+	public const FILTER_FIRSTFOURLETTER = 'firstFourLetter';
+	public const FILTER_FIRSTFIVELETTER = 'firstFiveLetter';
+    public const FILTER_SLUG = 'slug';
+    public const FILTER_JSON_ARRAY = 'jsonArray';
+    public const FILTER_JSON_OBJECT = 'jsonObject';
+	public const FILTER_COLORHEXA= 'colorHexa';
 
 
-    protected static $filters;
+    protected static $filters = array(
+        self::FILTER_DEFAULT,
+        self::FILTER_BOOLEAN,
+        self::FILTER_INT,
+        self::FILTER_FLOAT,
+        self::FILTER_ALPHA,
+        self::FILTER_SCALAR,
+        self::FILTER_MAIL,
+        self::FILTER_IP,
+        self::FILTER_MAC,
+        self::FILTER_URL,
+        self::FILTER_FIRSTLETTER,
+        self::FILTER_FIRSTTWOLETTER,
+        self::FILTER_FIRSTTHREELETTER,
+        self::FILTER_FIRSTFOURLETTER,
+        self::FILTER_FIRSTFIVELETTER,
+        self::FILTER_SLUG,
+        self::FILTER_JSON_ARRAY,
+        self::FILTER_JSON_OBJECT,
+        self::FILTER_COLORHEXA,
+    );
+    
+    
+    public function __construct()
+    {
+    }
 
 
-	/**
+    /**
 	 * @param string $rawValue
 	 * @param string $filter
 	 * @return mixed
 	 */
-	public static function get(string $rawValue,
-                               string $filter = self::FILTER_DEFAULT)
+	public function get(string $rawValue,
+                        string $filter = self::FILTER_DEFAULT)
     {
 		switch($filter) {
 			//PHP filter types
-			case self::FILTER_BOOLEAN: return filter_var($rawValue, FILTER_VALIDATE_BOOLEAN); break;
-			case self::FILTER_INT: return filter_var($rawValue, FILTER_VALIDATE_INT); break;
-			case self::FILTER_FLOAT: return filter_var($rawValue, FILTER_VALIDATE_FLOAT); break;
-			case self::FILTER_MAIL: return filter_var($rawValue, FILTER_VALIDATE_EMAIL); break;
-			case self::FILTER_IP: return filter_var($rawValue, FILTER_VALIDATE_IP); break;
-			case self::FILTER_MAC: return filter_var($rawValue, FILTER_VALIDATE_MAC); break;
-			case self::FILTER_REGEXP: return filter_var($rawValue, FILTER_VALIDATE_REGEXP); break;
-			case self::FILTER_URL: return filter_var($rawValue, FILTER_VALIDATE_URL); break;
+			case static::FILTER_BOOLEAN: return filter_var($rawValue, FILTER_VALIDATE_BOOLEAN); break;
+			case static::FILTER_INT: return filter_var($rawValue, FILTER_VALIDATE_INT); break;
+			case static::FILTER_FLOAT: return filter_var($rawValue, FILTER_VALIDATE_FLOAT); break;
+			case static::FILTER_MAIL: return filter_var($rawValue, FILTER_VALIDATE_EMAIL); break;
+			case static::FILTER_IP: return filter_var($rawValue, FILTER_VALIDATE_IP); break;
+			case static::FILTER_MAC: return filter_var($rawValue, FILTER_VALIDATE_MAC); break;
+			case static::FILTER_URL: return filter_var($rawValue, FILTER_VALIDATE_URL); break;
 
 			//Local types
-			case self::FILTER_FIRSTLETTER: return self::getFirstLetters($rawValue, 1); break;
-			case self::FILTER_FIRSTTWOLETTER: return self::getFirstLetters($rawValue, 2); break;
-			case self::FILTER_FIRSTTHREELETTER: return self::getFirstLetters($rawValue, 3); break;
-			case self::FILTER_FIRSTFOURLETTER: return self::getFirstLetters($rawValue, 4); break;
-			case self::FILTER_FIRSTFIVELETTER: return self::getFirstLetters($rawValue, 5); break;
-			case self::FILTER_SLUG: return self::getSlug($rawValue); break;
-			case self::FILTER_ALPHA: return self::getAlpha($rawValue); break;
-			case self::FILTER_SCALAR: return self::getScalar($rawValue); break;
-            case self::FILTER_JSON_ARRAY: return self::getJsonArray($rawValue); break;
-            case self::FILTER_JSON_OBJECT: return self::getJsonObject($rawValue); break;
-			case self::FILTER_COLORHEXA: return self::getColorHexa($rawValue); break;
+			case static::FILTER_FIRSTLETTER: return $this->getFirstLetters($rawValue, 1); break;
+			case static::FILTER_FIRSTTWOLETTER: return $this->getFirstLetters($rawValue, 2); break;
+			case static::FILTER_FIRSTTHREELETTER: return $this->getFirstLetters($rawValue, 3); break;
+			case static::FILTER_FIRSTFOURLETTER: return $this->getFirstLetters($rawValue, 4); break;
+			case static::FILTER_FIRSTFIVELETTER: return $this->getFirstLetters($rawValue, 5); break;
+			case static::FILTER_SLUG: return $this->getSlug($rawValue); break;
+			case static::FILTER_ALPHA: return $this->getAlpha($rawValue); break;
+			case static::FILTER_SCALAR: return $this->getScalar($rawValue); break;
+            case static::FILTER_JSON_ARRAY: return $this->getJsonArray($rawValue); break;
+            case static::FILTER_JSON_OBJECT: return $this->getJsonObject($rawValue); break;
+			case static::FILTER_COLORHEXA: return $this->getColorHexa($rawValue); break;
 		}
-		return self::getDefault($rawValue);
+		return $this->getDefault($rawValue);
 	}
 
 
@@ -73,10 +96,6 @@ class StringSanitizer
      */
     public static function getFilters()
     {
-        if(empty(static::$filters)) {
-            $rClass = new \ReflectionClass(static::class);
-            static::$filters = array_values($rClass->getConstants());
-        }
         return static::$filters;
     }
 
@@ -97,22 +116,29 @@ class StringSanitizer
 	 * @param string $filter
 	 * @return array
 	 */
-	public static function processArray(array $rawValues,
-                                        string $filter = self::FILTER_DEFAULT)
+	public function processArray(array $rawValues,
+                                 string $filter = self::FILTER_DEFAULT)
     {
 		$sanitized = array();
 		foreach($rawValues as $rawValue) {
-			$sanitizedValue = self::get($rawValue, $filter);
+			$sanitizedValue = $this->get($rawValue, $filter);
 			$sanitized[] = $sanitizedValue;
 		}
 		return $sanitized;
 	}
 
 
-	public static function getArray(string $rawValue, string $separator, string $filter = self::FILTER_DEFAULT)
+    /**
+     * sanitize all part of a string, converted to array according to separator
+     * @param string $rawValue
+     * @param string $separator
+     * @param string $filter
+     * @return array
+     */
+	public function getArray(string $rawValue, string $separator, string $filter = self::FILTER_DEFAULT)
     {
         $rawList = explode($separator, $rawValue);
-        return static::processArray($rawList, $filter);
+        return $this->processArray($rawList, $filter);
     }
 
 
@@ -120,7 +146,7 @@ class StringSanitizer
 	 * @param string $rawValue
 	 * @return string
 	 */
-	protected static function getDefault(string $rawValue)
+	protected function getDefault(string $rawValue)
     {
 		return htmlentities(strip_tags($rawValue));
 	}
@@ -134,11 +160,11 @@ class StringSanitizer
 	 * @param int $count
 	 * @return string
 	 */
-	protected static function sanitizeRawValue(string $rawValue,
-                                               string $pattern,
-                                               string $replacement = '$1',
-                                               int $limit = -1,
-                                               int &$count = 0)
+	protected function sanitizeRawValue(string $rawValue,
+                                        string $pattern,
+                                        string $replacement = '$1',
+                                        int $limit = -1,
+                                        int &$count = 0)
     {
 		if(preg_match($pattern, $rawValue)) {
 			$temp = preg_replace($pattern, $replacement, $rawValue, $limit, $count);
@@ -155,18 +181,18 @@ class StringSanitizer
 	 * @param string $rawValue
 	 * @return string
 	 */
-	protected static function getAlpha(string $rawValue)
+	protected function getAlpha(string $rawValue)
     {
-		return self::sanitizeRawValue($rawValue, '/^([a-zA-Z]+)(.*)/');
+		return $this->sanitizeRawValue($rawValue, '/^([a-zA-Z]+)(.*)/');
 	}
 
 	/**
 	 * @param string $rawValue
 	 * @return string
 	 */
-	protected static function getScalar(string $rawValue)
+	protected function getScalar(string $rawValue)
     {
-		return self::sanitizeRawValue($rawValue, '/^([a-zA-Z0-9]+)(.*)/');
+		return $this->sanitizeRawValue($rawValue, '/^([a-zA-Z0-9]+)(.*)/');
 	}
 
 	/**
@@ -174,36 +200,36 @@ class StringSanitizer
 	 * @param int $numberMax
 	 * @return string
 	 */
-	protected static function getFirstLetters(string $rawValue, int $numberMax = 1)
+	protected function getFirstLetters(string $rawValue, int $numberMax = 1)
     {
-		return self::sanitizeRawValue($rawValue, '/^([a-zA-Z]{1,' . $numberMax . '})(.*)/');
+		return $this->sanitizeRawValue($rawValue, '/^([a-zA-Z]{1,' . $numberMax . '})(.*)/');
 	}
 
 	/**
 	 * @param string $rawValue
 	 * @return string
 	 */
-	protected static function getSlug(string $rawValue)
+	protected function getSlug(string $rawValue)
     {
-		return self::sanitizeRawValue($rawValue, '/^([a-zA-Z0-9!\/\-\._]+)(.*)/');
+		return $this->sanitizeRawValue($rawValue, '/^([a-zA-Z0-9_]+)(.*)/');
 	}
 
     /**
      * @param string $rawValue
      * @return array
      */
-    protected static function getJsonArray(string $rawValue)
+    protected function getJsonArray(string $rawValue)
     {
-        return self::getJson($rawValue, true);
+        return $this->getJson($rawValue, true);
     }
 
     /**
      * @param string $rawValue
      * @return object
      */
-    protected static function getJsonObject(string $rawValue)
+    protected function getJsonObject(string $rawValue)
     {
-        return self::getJson($rawValue, false);
+        return $this->getJson($rawValue, false);
     }
 
     /**
@@ -211,7 +237,7 @@ class StringSanitizer
      * @param bool $assoc
      * @return mixed
      */
-    private static function getJson(string $rawValue, bool $assoc = true)
+    private function getJson(string $rawValue, bool $assoc = true)
     {
         $json = json_decode($rawValue, $assoc);
         if(JSON_ERROR_NONE == json_last_error()) {
@@ -222,12 +248,29 @@ class StringSanitizer
 
 
     /**
+     * # char is not mandatory.
+     * 'short-code' on 3 chars are allowed, but sanitized on 6 (ie 'FFF' will give '#FFFFFF')
 	 * @param string $rawValue
 	 * @return string
 	 */
-	protected static function getColorHexa(string $rawValue)
+	protected function getColorHexa(string $rawValue)
     {
-		return '#' . self::sanitizeRawValue($rawValue, '/^#?([a-fA-F0-9]{6})(.*)/');
+        $sanitized = $this->sanitizeRawValue($rawValue, '/^#?([a-fA-F0-9]{6})(.*)/');
+        if(empty($sanitized)) {
+            //try with 'short code' with 3 chars only
+            $sanitized = $this->sanitizeRawValue($rawValue, '/^#?([a-fA-F0-9]{3})(.*)/');
+            if(empty($sanitized)) {
+                return '';
+            }
+
+            //rebuild full code
+            $sanitizedChars = str_split($sanitized);
+            $sanitized = '';
+            foreach($sanitizedChars as $char) {
+                $sanitized .= $char . $char;
+            }
+        }
+        return '#' . $sanitized;
 	}
 
 }
