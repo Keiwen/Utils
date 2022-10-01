5112c6b285364c48a427d276c2fe88435d0f7c1d
Keiwen <keiwen.mk@gmail.com>
2022-10-01 22:12:15 +0200

    adding iso date format

--- Format/DateFormat.php
+++ Format/DateFormat.php
@@ -38,6 +38,7 @@ class DateFormat
      */
     public function formatDateAndTime($value, int $datetype = null, int $timetype = null)
     {
+        if (empty($value)) $value = time();
         return $this->getFormatter($datetype, $timetype)->format($value);
     }
 
@@ -167,6 +168,58 @@ class DateFormat
     public function formatDateAndTimeShort($value)
     {
         return $this->formatDateAndTime($value, \IntlDateFormatter::SHORT, \IntlDateFormatter::SHORT);
+    }
+
+    /**
+     * @param $value
+     * @return \DateTime
+     */
+    protected function getDateTime($value)
+    {
+        try {
+            if($value instanceof \DateTime) {
+                $dateTime = $value;
+            } elseif ($value instanceof \IntlCalendar) {
+                $init = round(($value->getTime()) / 1000);
+                $dateTime = new \DateTime('');
+                $dateTime->setTimestamp($init);
+            } else {
+                if($value === '') $value = 'now';
+                $init = is_string($value) ? $value : 'now';
+                $dateTime = new \DateTime($init);
+                if(is_int($value)) $dateTime->setTimestamp($value);
+            }
+        } catch (\Exception $e) {
+            $dateTime = new \DateTime('now');
+        }
+        return $dateTime;
+    }
+
+    /**
+     * @param $value
+     * @return string
+     */
+    public function formatDateAndTimeIso($value)
+    {
+        return $this->getDateTime($value)->format('c');
+    }
+
+    /**
+     * @param $value
+     * @return string
+     */
+    public function formatDateIso($value)
+    {
+        return $this->getDateTime($value)->format('Y-m-d');
+    }
+
+    /**
+     * @param $value
+     * @return string
+     */
+    public function formatTimeIso($value)
+    {
+        return $this->getDateTime($value)->format('H:i:s');
     }
 
 }
