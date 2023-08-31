9d4c5fb53c0105348fb780d040d406463854bfff
keiwen <keiwen.mk@gmail.com>
2023-08-31 09:35:24 +0200

    store starting order, result and performance

--- Competition/AbstractGame.php
+++ Competition/AbstractGame.php
@@ -5,10 +5,81 @@ namespace Keiwen\Utils\Competition;
 abstract class AbstractGame
 {
 
+    protected $players = array();
+    protected $results = array();
+    protected $performances = array();
     protected $gameNumber = 0;
     protected $played = false;
     protected $affected = false;
     protected $affectedTo = null;
+
+
+    protected function setPlayers(array $idPlayersOrd)
+    {
+        $inputCount = count($idPlayersOrd);
+        $players = array_flip(array_combine(range(1, count($idPlayersOrd)), array_values($idPlayersOrd)));
+        if ($inputCount != count($players)) {
+            throw new CompetitionException('Unable to find unique players on start order, check for duplicates');
+        }
+        $this->players = $players;
+    }
+
+
+    /**
+     * @param int $idPlayer
+     * @return int 0 if not found
+     */
+    public function getPlayerStartingOrd(int $idPlayer)
+    {
+        return $this->players[$idPlayer] ?? 0;
+    }
+
+
+    /**
+     * @param int $ord
+     * @return int id player or 0 if not found
+     */
+    public function getPlayerThatStartedAt(int $ord)
+    {
+        $playersPositions = array_flip($this->players);
+        return $playersPositions[$ord] ?? 0;
+    }
+
+    /**
+     * @return array idPlayer => starting ord
+     */
+    public function getStartingOrd()
+    {
+        return $this->players;
+    }
+
+
+    /**
+     * @param int $idPlayer
+     * @param mixed $result
+     */
+    protected function setPlayerResult(int $idPlayer, $result)
+    {
+        $this->results[$idPlayer] = $result;
+    }
+
+    /**
+     * @param int $idPlayer
+     * @return mixed|null null if not found
+     */
+    public function getPlayerResult(int $idPlayer)
+    {
+        return $this->results[$idPlayer] ?? null;
+    }
+
+    /**
+     * @return array idPlayer => result
+     */
+    public function getResults(): array
+    {
+        return $this->results;
+    }
+
 
     /**
      * @return int
@@ -36,6 +107,32 @@ abstract class AbstractGame
     public function isPlayed(): bool
     {
         return $this->played;
+    }
+
+    /**
+     * @param int $idPlayer
+     * @param mixed $performance
+     */
+    protected function setPlayerPerformance(int $idPlayer, $performance)
+    {
+        $this->performances[$idPlayer] = $performance;
+    }
+
+    /**
+     * @param int $idPlayer
+     * @return mixed|null null if not found
+     */
+    public function getPlayerPerformance(int $idPlayer)
+    {
+        return $this->performances[$idPlayer] ?? null;
+    }
+
+    /**
+     * @return array idPlayer => performance
+     */
+    public function getPerformances(): array
+    {
+        return $this->performances;
     }
 
     public function isAffected(): bool
