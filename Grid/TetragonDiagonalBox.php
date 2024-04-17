032ae0c696d7bea6519e379c50c5ed872d19a71a
keiwen <keiwen.mk@gmail.com>
2024-04-17 10:21:28 +0200

    feat: rely on adjust coord global method

--- Grid/TetragonDiagonalBox.php
+++ Grid/TetragonDiagonalBox.php
@@ -19,57 +19,22 @@ class TetragonDiagonalBox extends TetragonBox
             case AbstractGrid::DIRECTION_UPRIGHT:
                 $neighborsCoord[0]--;
                 $neighborsCoord[1]++;
-                //if "out of grid"
-                if(!$this->grid->isOnGrid($neighborsCoord)) {
-                    if($this->grid->hasBorder()) {
-                        //we have border => no neighbor
-                        return null;
-                    }
-                    if($neighborsCoord[0] < 0) $neighborsCoord[0] = $this->grid->getMaxHeight() - 1;
-                    if($neighborsCoord[1] >= $this->grid->getMaxWidth()) $neighborsCoord[1] = 0;
-                }
                 break;
             case AbstractGrid::DIRECTION_DOWNRIGHT:
                 $neighborsCoord[0]++;
                 $neighborsCoord[1]++;
-                //if "out of grid"
-                if(!$this->grid->isOnGrid($neighborsCoord)) {
-                    if($this->grid->hasBorder()) {
-                        //we have border => no neighbor
-                        return null;
-                    }
-                    if($neighborsCoord[0] >= $this->grid->getMaxHeight()) $neighborsCoord[0] = 0;
-                    if($neighborsCoord[1] >= $this->grid->getMaxWidth()) $neighborsCoord[1] = 0;
-                }
                 break;
             case AbstractGrid::DIRECTION_DOWNLEFT:
                 $neighborsCoord[0]++;
                 $neighborsCoord[1]--;
-                //if "out of grid"
-                if(!$this->grid->isOnGrid($neighborsCoord)) {
-                    if($this->grid->hasBorder()) {
-                        //we have border => no neighbor
-                        return null;
-                    }
-                    if($neighborsCoord[0] >= $this->grid->getMaxHeight()) $neighborsCoord[0] = 0;
-                    if($neighborsCoord[1] < 0) $neighborsCoord[1] = $this->grid->getMaxWidth() - 1;
-                }
                 break;
             case AbstractGrid::DIRECTION_UPLEFT:
                 $neighborsCoord[0]--;
                 $neighborsCoord[1]--;
-                //if "out of grid"
-                if(!$this->grid->isOnGrid($neighborsCoord)) {
-                    if($this->grid->hasBorder()) {
-                        //we have border => no neighbor
-                        return null;
-                    }
-                    if($neighborsCoord[0] < 0) $neighborsCoord[0] = $this->grid->getMaxHeight() - 1;
-                    if($neighborsCoord[1] < 0) $neighborsCoord[1] = $this->grid->getMaxWidth() - 1;
-                }
                 break;
             default: return null;
         }
+        $neighborsCoord = $this->grid->adjustCoord($neighborsCoord);
         if(!TetragonDiagonalGrid::isValidCoord($neighborsCoord)) return null;
         return $this->grid->getBox($neighborsCoord);
     }
