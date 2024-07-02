3b7422c7028a785070a5ba4fb6e59f39512e0930
keiwen <keiwen.mk@gmail.com>
2024-07-02 23:41:34 +0200

    feat: introducing builder tree/phase to architecture complex/nested competitions

---
+++ Competition/CompetitionBuilderTree.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Keiwen\Utils\Competition;
+
+class CompetitionBuilderTree
+{
+
+    /** @var string */
+    protected $name = '';
+    /** @var CompetitionBuilderPhase[] */
+    protected $builderPhases = array();
+    protected $iterations = array();
+
+    const PLAYER_PACK_QUALIFIED = 'qualified';
+    const PLAYER_PACK_STAGNATION = 'stagnation';
+    const PLAYER_PACK_UNUSED = 'unused';
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @param string $name
+     * @return CompetitionBuilderPhase
+     */
+    public function addPhase(string $name): CompetitionBuilderPhase
+    {
+        $phase = new CompetitionBuilderPhase($name);
+        $this->builderPhases[$name] = $phase;
+        return $phase;
+    }
+
+    public function getPhase(string $name): ?CompetitionBuilderPhase
+    {
+        return $this->builderPhases[$name] ?? null;
+    }
+
+
+    public function getPhaseAfter(string $name): ?CompetitionBuilderPhase
+    {
+        $phaseNames = array_keys($this->builderPhases);
+        $searchIndex = array_search($name, $phaseNames, true);
+        // if given phase not found, nothing after
+        if ($searchIndex === false) return null;
+        // if next index out of bond, means that no further phase
+        if (count($phaseNames) < ($searchIndex + 1)) return null;
+
+        $nextPhaseName = $phaseNames[$searchIndex + 1];
+        return $this->getPhase($nextPhaseName);
+    }
+
+    /**
+     * @return CompetitionTreePhase[]
+     */
+    public function getPhases(): array
+    {
+        return $this->builderPhases;
+    }
+
+
+    /**
+     * @param array $players
+     * @param string $iterationName
+     * @return CompetitionTree|null
+     */
+    public function startIteration(array $players, string $iterationName = ''): ?CompetitionTree
+    {
+        if (empty($this->builderPhases)) return null;
+
+        $iteration = new CompetitionTree($this, $players, $iterationName);
+        $this->iterations[$iterationName] = $iteration;
+        return $iteration;
+    }
+
+
+    /**
+     * @param string $iterationName
+     * @return CompetitionTree|null null if not found
+     */
+    public function getIteration(string $iterationName = ''): ?CompetitionTree
+    {
+        return $this->iterations[$iterationName] ?? null;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+
+}
